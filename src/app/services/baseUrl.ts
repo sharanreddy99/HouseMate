070a6609a2e9af65db177e55090499527ce337cb
Sharan Reddy <sharansaikonda99@gmail.com>
2020-11-04 08:43:46 +0530

    commit for heroku

--- src/app/services/baseUrl.ts
+++ src/app/services/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const baseUrl: string = "/api/"; //For AWS Deployment
-export const baseUrl: string = "http://localhost:4201/api/"; //for local server deployment
+export const baseUrl: string = "/api/"; //For AWS Deployment
+//export const baseUrl: string = "http://localhost:4201/api/"; //for local server deployment
