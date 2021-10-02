f34ad5080cefc7115fa67f5a9ebe5e27b392d724
Sharan Konda <sharansai.konda@sureify.com>
2021-10-02 19:12:16 +0530

    fix bugs

--- backend/src/db/migrate-mongo-config.js
+++ backend/src/db/migrate-mongo-config.js
@@ -2,11 +2,9 @@
 
 const config = {
   mongodb: {
-    // TODO Change (or review) the url to your MongoDB:
-    url: "mongodb://localhost:27017",
+    url: "HOUSEMATE_DATABASE_URL",
 
-    // TODO Change this to your database name:
-    databaseName: "housematedb",
+    databaseName: "HOUSEMATE_DATABASE_NAME",
 
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
